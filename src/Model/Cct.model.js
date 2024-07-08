73d800f49749f96f5060f0d053c29ee71549bd1f
taufik69 <taufik35-2915@diu.edu.bd>
2024-07-09 02:35:09 +0600

    @Schema done without product Shcema

--- src/Model/Cct.model.js
+++ src/Model/Cct.model.js
@@ -1,14 +1,15 @@
 import mongoose, { Schema } from "mongoose";
-const ipListSchema = new Schema(
+
+const cctSchema = new Schema(
   {
     Title: {
       type: String,
       trim: true,
-      required: [true, "Missing U Crendential"],
+      required: [true, "Missing cct Crendential"],
       lowercase: true,
     },
   },
   { timestamps: true }
 );
 
-export const ipListModel = mongoose.model("ipList", ipListSchema);
+export const cctModel = mongoose.model("cct", cctSchema);
