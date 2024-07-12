fe055c143725e9178bac31e16f8185eea6f3d97d
taufik69 <taufik35-2915@diu.edu.bd>
2024-07-13 03:28:03 +0600

    @env file change add new env variable compnay name route

--- src/Routes/api/index.apiRoutes.js
+++ src/Routes/api/index.apiRoutes.js
@@ -4,9 +4,9 @@ import iplistRoutes from "./iplist.apiRoutes.js";
 import beamAngleRoutes from "./beamAngle.apiRoutes.js";
 import reflectorTypeRoutes from "./reflectorType.apiRoutes.js";
 const router = Router();
-router.use("/bdluminers", utilsRoutes);
-router.use("/bdluminers", iplistRoutes);
-router.use("/bdluminers", beamAngleRoutes);
-router.use("/bdluminers", reflectorTypeRoutes);
+router.use(process.env.CONPANY_NAME_ROUTE, utilsRoutes);
+router.use(process.env.CONPANY_NAME_ROUTE, iplistRoutes);
+router.use(process.env.CONPANY_NAME_ROUTE, beamAngleRoutes);
+router.use(process.env.CONPANY_NAME_ROUTE, reflectorTypeRoutes);
 
 export default router;
