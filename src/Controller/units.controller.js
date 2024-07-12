f26480eddb516550f535b1e8881bb08d0bd30eaa
taufik69 <taufik35-2915@diu.edu.bd>
2024-07-13 01:58:06 +0600

    @unit controller done sucessfully put data into database and check if title is already exist does not put same unit

--- src/Controller/units.controller.js
+++ src/Controller/units.controller.js
@@ -1,7 +1,36 @@
 import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-const unitsController = asyncHandler((req, res, next) => {
-  res.status(200).json(new ApiResponse(200, null, "EveryThing is Ok"));
+import { ApiError } from "../utils/ApiError.js";
+import { unitModel } from "../Model/Unit.model.js";
+import chalk from "chalk";
+const unitsController = asyncHandler(async (req, res, next) => {
+  const { Title } = req.body;
+  //   console.log(new ApiError(401, null, "Unit Title Is Missing"));
+  if (!Title) {
+    return res
+      .status(400)
+      .json(new ApiError(401, null, "Unit Title Is Missing"));
+  }
+  /**
+   * todo : check already have a same units
+   */
+  const isExistUnit = await unitModel.find({ Title });
+  if (isExistUnit.length) {
+    return res
+      .status(400)
+      .json(new ApiError(401, null, "Unit Title Is Already Exist"));
+  }
+  /**
+   * todo : make a new unit
+   */
+
+  const unit = await new unitModel({
+    Title,
+  }).save();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, unit, "unit Created Sucessfull is Ok"));
 });
 
 export { unitsController };
