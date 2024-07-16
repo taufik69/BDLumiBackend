1e9c869da1eab72d7c13e1c830e3ee232a48ce90
taufik69 <taufik35-2915@diu.edu.bd>
2024-07-17 04:15:26 +0600

    @model product itemcode field required

--- src/Model/Product.model.js
+++ src/Model/Product.model.js
@@ -6,7 +6,7 @@ const productSchema = new Schema(
       type: String,
       trim: true,
       required: [true, "Missing product"],
-      lowercase: true,
+      unique: true,
     },
     productImages: {
       type: String,
